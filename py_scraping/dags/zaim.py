7829d798e19e1126dd10d93fd4fe64f01f5a5b77
Takaaki Tanaka <ttanaka@Lugh.local>
2017-11-04 00:21:08 +0900

    correct typo

--- py_scraping/dags/zaim.py
+++ py_scraping/dags/zaim.py
@@ -11,7 +11,7 @@ dag = DAG('zaim', default_args=default_args, schedule_interval='00 00 * * *')
 
 task1 = BashOperator(
     task_id='retrieve_zaim_data',
-    bash_command='python /app/zaim_downLoader.py',
+    bash_command='python /app/zaim_downloader.py',
     dag=dag)
 
 task2 = BashOperator(
